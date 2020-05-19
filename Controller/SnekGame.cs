ed7d994b1c202da0e78c77c55931327a31157402
Andrew Hoeft <ahoeft9@gmail.com>
2020-05-19 12:06:57 -0500

    add more boiler plate to SnekGame.cs

--- Controller/SnekGame.cs
+++ Controller/SnekGame.cs
@@ -3,6 +3,7 @@ namespace Snek
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework.Input;
+    using Controller;
     using View.Renderers;
 
     public class SnekGame : Game
@@ -25,8 +26,9 @@ namespace Snek
 
         protected override void Initialize()
         {
+            StateMachine.Initialize();
+            StateMachine.ChangeState();
             base.Initialize();
-
         }
 
         protected override void LoadContent()
@@ -37,12 +39,16 @@ namespace Snek
         protected override void Update(GameTime gameTime)
         {
             Elapsed = (double) gameTime.ElapsedGameTime.TotalSeconds;
+            StateMachine.CurrentState.Execute();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            SpriteBatch.Begin();
+            StateMachine.CurrentState.Draw(Renderer);
+            SpriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- Controller/StateMachine.cs
+++ Controller/StateMachine.cs
@@ -5,8 +5,12 @@ namespace Snek.Controller
     public static class StateMachine 
     {
         public static State CurrentState;
-        public static void Initialize () {}
-        public static void ChangeState() {}
+        public static void Initialize () {
+            throw new System.NotImplementedException();
+        }
+        public static void ChangeState() {
+            throw new System.NotImplementedException();
+        }
     }
     
 }
